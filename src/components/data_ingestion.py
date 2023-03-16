7de9a8769bf3b82b62b05919da4fa677e9d96a84
Souvik Misra <svkmsr6@gmail.com>
2023-03-16 22:44:26 +0530

    Artifacts added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,9 +9,9 @@ from src.exception import CustomException
 from src.logger import logging
 
 class DataIngestionConfig:
-    train_data_path:str = os.path("artifacts","train.csv")
-    test_data_path:str = os.path("artifacts","test.csv")
-    raw_data_path:str = os.path("artifacts","raw.csv")
+    train_data_path:str = os.path.join("artifacts","train.csv")
+    test_data_path:str = os.path.join("artifacts","test.csv")
+    raw_data_path:str = os.path.join("artifacts","raw.csv")
 
 class DataIngestion:
     def __init__(self):
